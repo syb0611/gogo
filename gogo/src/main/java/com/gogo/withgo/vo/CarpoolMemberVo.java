3bf989eb33863d85c0ca300088f62d58af7e5088
bitbit <bitbit@bit>
2015-11-10 17:10:20 +0900

    n

---
+++ gogo/src/main/java/com/gogo/withgo/vo/CarpoolMemberVo.java
@@ -0,0 +1,227 @@
+package com.gogo.withgo.vo;
+
+import java.sql.Date;
+
+public class CarpoolMemberVo {
+	private int carno;			
+	private String category;	
+	private int mno;			
+	private String departure;	
+	private String arrival;		
+	private String usertype;  	
+	private int seat;  		
+	private String memo;		
+	private Date wdate;		
+	private String email;
+	private String pw;
+	private String nickname;
+	private String name;
+	private String phone;
+	private String birth;
+	private int gender;
+	private int hascar;
+	private String image;
+	private Date jdate;
+	private String profile;
+	private String auth;
+	
+	public CarpoolMemberVo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public CarpoolMemberVo(int carno, String category, int mno, String departure, String arrival, String usertype, int seat,
+			String memo, Date wdate, String email, String pw, String nickname, String name, String phone, String birth,
+			int gender, int hascar, String image, Date jdate, String profile, String auth) {
+		super();
+		this.carno = carno;
+		this.category = category;
+		this.mno = mno;
+		this.departure = departure;
+		this.arrival = arrival;
+		this.usertype = usertype;
+		this.seat = seat;
+		this.memo = memo;
+		this.wdate = wdate;
+		this.email = email;
+		this.pw = pw;
+		this.nickname = nickname;
+		this.name = name;
+		this.phone = phone;
+		this.birth = birth;
+		this.gender = gender;
+		this.hascar = hascar;
+		this.image = image;
+		this.jdate = jdate;
+		this.profile = profile;
+		this.auth = auth;
+	}
+
+	public int getCarno() {
+		return carno;
+	}
+
+	public void setCarno(int carno) {
+		this.carno = carno;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public int getMno() {
+		return mno;
+	}
+
+	public void setMno(int mno) {
+		this.mno = mno;
+	}
+
+	public String getDeparture() {
+		return departure;
+	}
+
+	public void setDeparture(String departure) {
+		this.departure = departure;
+	}
+
+	public String getArrival() {
+		return arrival;
+	}
+
+	public void setArrival(String arrival) {
+		this.arrival = arrival;
+	}
+
+	public String getUsertype() {
+		return usertype;
+	}
+
+	public void setUsertype(String usertype) {
+		this.usertype = usertype;
+	}
+
+	public int getSeat() {
+		return seat;
+	}
+
+	public void setSeat(int seat) {
+		this.seat = seat;
+	}
+
+	public String getMemo() {
+		return memo;
+	}
+
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+
+	public Date getWdate() {
+		return wdate;
+	}
+
+	public void setWdate(Date wdate) {
+		this.wdate = wdate;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getBirth() {
+		return birth;
+	}
+
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+
+	public int getGender() {
+		return gender;
+	}
+
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+
+	public int getHascar() {
+		return hascar;
+	}
+
+	public void setHascar(int hascar) {
+		this.hascar = hascar;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public Date getJdate() {
+		return jdate;
+	}
+
+	public void setJdate(Date jdate) {
+		this.jdate = jdate;
+	}
+
+	public String getProfile() {
+		return profile;
+	}
+
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+
+	public String getAuth() {
+		return auth;
+	}
+
+	public void setAuth(String auth) {
+		this.auth = auth;
+	}
+	
+}
---
+++ gogo/src/main/webapp/WEB-INF/views/myInfo.jsp
@@ -0,0 +1,98 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+<meta charset="utf-8">
+<title>Contact - Grill Template</title>
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width">
+
+<link
+	href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'
+	rel='stylesheet' type='text/css'>
+
+<link rel="stylesheet" href="../css/bootstrap.css">
+<link rel="stylesheet" href="../css/font-awesome.css">
+<link rel="stylesheet" href="../css/templatemo_style.css">
+<link rel="stylesheet" href="../css/templatemo_misc.css">
+<link rel="stylesheet" href="../css/flexslider.css">
+<link rel="stylesheet" href="../css/testimonails-slider.css">
+
+<style>
+#heading {
+/* 	background-image: url(images/community.png); */
+	background-color: gray;
+	height: 140px;
+}
+
+.content{
+	margin: 30px;
+}
+
+
+
+</style>
+<script src="js/vendor/modernizr-2.6.1-respond-1.1.0.min.js"></script>
+</head>
+<body>
+	<header>
+		<%@ include file="header.jsp"%>
+		<div id="main-header">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3">
+						<div class="logo">
+							<a href="${contextPath}/home"><img src="../images/pool2.jpeg" title="Grill Template" alt="Grill Website Template"></a>
+						</div>
+					</div>
+					<div class="col-md-7">
+						<div class="main-menu">
+							<ul>
+								<li><a href="dan"><font size=3>단기카풀</font></a></li>
+								<li><a href="jang"><font size=3>장기카풀</font></a></li>
+								<li><a href="goschool"><font size=3>고 스쿨</font></a></li>
+								<li><a href="taxi"><font size=3>택시카풀</font></a></li>
+								<li><a href="${contextPath}/board/main"><font size=3>커뮤니티</font></a></li>
+								<li><a href="customer"><font size=3>고객센터</font></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+
+	<div id="heading">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="heading-content">
+						<h2>MYINFO</h2>
+						<span>Home / 내정보 </span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="content">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1">	
+				내정보
+			</div>		
+		</div>		
+	</div>
+	</div>
+
+	<%@ include file="footer.jsp"%>
+
+	<script src="js/vendor/jquery-1.11.0.min.js"></script>
+	<script src="js/vendor/jquery.gmap3.min.js"></script>
+	<script src="js/plugins.js"></script>
+	<script src="js/main.js"></script>
+
+</body>
+</html>
