7f9ed3c35bbc8a4c6902aae765b75afdfdce959f
Dubu <Dubu@BitDubu>
2015-11-24 13:54:23 +0900

    몽고디비

--- gogo/src/main/java/com/gogo/withgo/controller/MapMongoController.java
+++ gogo/src/main/java/com/gogo/withgo/controller/MapMongoController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -48,5 +49,11 @@ public class MapMongoController {
 		mapResult.put( "data", find );
 
 		return mapResult;
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="/markertest/{seq}", method=RequestMethod.GET)
+	public String setRotue(@PathVariable int seq){
+		return dao.findOne(seq).toString();
 	}
 }
